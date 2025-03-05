a3f5c88ecf61294b72460de8d729aa1a7ae6ea14
clawdeenhsia <91250836+clawdeenhsia@users.noreply.github.com>
2025-03-05 11:50:14 +0800

    week03

---
+++ week03-4.java
@@ -0,0 +1,16 @@
+//week03-4-mousePressed-mouseX-mouseY=float-x-y
+void setup(){
+  size(400,400);
+
+}
+float x=200,y=200;
+void draw(){
+  //if(mousePressed)ellipse(mouseX,mouseY,50,30);
+  background(#5DD7FA);
+  if(mousePressed){
+    x = (x + mouseX) / 2;
+    y = (y + mouseY) / 2;
+  }
+  ellipse(x,y,50,30);
+}
+
